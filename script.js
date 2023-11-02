683e71bd79afce784ebbe924878a46bdfcbd6297
ayushig05 <ayushigupta0415@gmail.com>
2023-11-02 14:57:53 +0530

    added login/signup button

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function home() {
     contacts.style.display = "block";
     service.style.display = "flex";
     discount.style.display = "block";
+    loginForm.style.display = "none";
+    signupForm.style.display = "none";
     document.getElementById("home").style.color = "rgb(138, 110, 71)";
     document.getElementById("blog").style.color = "black";
     document.getElementById("category").style.color = "black";
@@ -29,6 +31,8 @@ function category() {
     contacts.style.display = "none";
     service.style.display = "none";
     discount.style.display = "none";
+    loginForm.style.display = "none";
+    signupForm.style.display = "none";
     document.getElementById("category").style.color = "rgb(138, 110, 71)";
     document.getElementById("home").style.color = "black";
     document.getElementById("blog").style.color = "black";
@@ -44,6 +48,8 @@ function blogs() {
     contacts.style.display = "none";
     service.style.display = "none";
     discount.style.display = "none";
+    loginForm.style.display = "none";
+    signupForm.style.display = "none";
     document.getElementById("blog").style.color = "rgb(138, 110, 71)";
     document.getElementById("home").style.color = "black";
     document.getElementById("category").style.color = "black";
@@ -59,6 +65,8 @@ function services() {
     contacts.style.display = "none";
     service.style.display = "flex";
     discount.style.display = "none";
+    loginForm.style.display = "none";
+    signupForm.style.display = "none";
     document.getElementById("services").style.color = "rgb(138, 110, 71)";
     document.getElementById("home").style.color = "black";
     document.getElementById("category").style.color = "black";
@@ -73,6 +81,8 @@ function contact() {
     blog.style.display = "none";
     contacts.style.display = "block";
     discount.style.display = "none";
+    loginForm.style.display = "none";
+    signupForm.style.display = "none";
     document.getElementById("contact").style.color = "rgb(138, 110, 71)";
     document.getElementById("home").style.color = "black";
     document.getElementById("category").style.color = "black";
@@ -84,7 +94,6 @@ let x;
 function show(x) {
     let newImg = document.getElementsByClassName("newimg");
     console.log(x.src);
-    // newImg.attribute("src") = "./images/dress5.webp";
     newImg.src = x.src;
     console.log(newImg.src);
     document.querySelector(".cart").style.display = "flex";
@@ -95,9 +104,134 @@ function show(x) {
     contacts.style.display = "none";
     service.style.display = "none";
     discount.style.display = "none";
+    loginForm.style.display = "none";
+    signupForm.style.display = "none";
 }
 
 function addcart() {
     alert("Added to cart");
     location.reload();
 }
+
+let loginForm = document.getElementById("login-form");
+let signupForm = document.getElementById("signup-form");
+let loginButton = document.getElementById("form-open");
+let loginLink = document.getElementById("login");
+let signupLink = document.getElementById("signup");
+
+function form() {
+    loginForm.style.display = "flex";
+    signupForm.style.display = "none";
+    mainpg.style.display = "none";
+    card.style.display = "none";
+    card2.style.display = "none";
+    blog.style.display = "none";
+    contacts.style.display = "none";
+    service.style.display = "none";
+    discount.style.display = "none";
+    document.getElementById("home").style.color = "black";
+    document.getElementById("blog").style.color = "black";
+    document.getElementById("category").style.color = "black";
+    document.getElementById("services").style.color = "black";
+    document.getElementById("contact").style.color = "black";
+}
+loginButton.addEventListener("click", form);
+signupLink.addEventListener("click", function (event) {
+    event.preventDefault(); 
+    signupForm.style.display = "flex";
+    loginForm.style.display = "none";
+    mainpg.style.display = "none";
+    card.style.display = "none";
+    card2.style.display = "none";
+    blog.style.display = "none";
+    contacts.style.display = "none";
+    service.style.display = "none";
+    discount.style.display = "none";
+    document.getElementById("home").style.color = "black";
+    document.getElementById("blog").style.color = "black";
+    document.getElementById("category").style.color = "black";
+    document.getElementById("services").style.color = "black";
+    document.getElementById("contact").style.color = "black";
+});
+
+function check() {
+    let email_login = document.formfill.Email.value;
+    let password_login = document.formfill.Password.value;
+    let result = document.querySelector(".result");
+    result.innerHTML = "";
+    if (email_login === "") {
+        result.innerHTML = "Enter your Email or Phone Number*";
+        return false;
+    }
+    else if (password_login === "") {
+        result.innerHTML = "Enter your Password*";
+        return false;
+    } 
+    else if (password_login.length < 6) {
+        result.innerHTML = "Password must be at least 6 characters*";
+        return false;
+    }
+    return true;
+}
+
+function validation() {
+    let username = document.FillForm.Name.value;
+    let email = document.FillForm.Email.value;
+    let password = document.FillForm.Password.value;
+    let confirmPassword = document.FillForm.CPassword.value;
+    let resultElement = document.querySelector(".result");
+    resultElement.innerHTML = "";
+    if (username === "") {
+        resultElement.innerHTML = "Enter Name*";
+        return false;
+    } 
+    else if (username.length < 6) {
+        resultElement.innerHTML = "Name must be at least six characters*";
+        return false;
+    } 
+    else if (email === "") {
+        resultElement.innerHTML = "Enter your Email or Phone Number*";
+        return false;
+    } 
+    else if (password === "") {
+        resultElement.innerHTML = "Enter your Password*";
+        return false;
+    } 
+    else if (password.length < 6) {
+        resultElement.innerHTML = "Password must be at least 6 characters*";
+        return false;
+    } 
+    else if (confirmPassword === "") {
+        resultElement.innerHTML = "Enter Confirm Password*";
+        return false;
+    } 
+    else if (confirmPassword !== password) {
+        resultElement.innerHTML = "Password doesn't match*";
+        return false;
+    }
+    return true;
+}
+
+function togglePassword(icon) {
+    const passwordField = icon.previousElementSibling;
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    } else {
+        passwordField.type = "password";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    }
+}
+
+loginLink.addEventListener("click", function form(x) {
+    x.preventDefault();
+    loginForm.style.display = "block";
+    signupForm.style.display = "none";
+});
+signupLink.addEventListener("click", function form(x) {
+    x.preventDefault();
+    loginForm.style.display = "none";
+    signupForm.style.display = "block";
+});
